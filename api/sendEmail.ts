5dcd862a02f9835079a88d10cc2e31686b05ea59
yanaaarev <yannahrevellame@gmail.com>
2025-09-26 16:08:33 +0800

    fix for sendemail6

--- api/sendEmail.ts
+++ api/sendEmail.ts
@@ -18,7 +18,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     });
 
     await transporter.sendMail({
-      from: `"Insta PayFlow HR" <${process.env.GMAIL_USER}>`,
+      from: process.env.GMAIL_USER,
       to,
       subject,
       text: text || "",   // fallback in case html not provided
